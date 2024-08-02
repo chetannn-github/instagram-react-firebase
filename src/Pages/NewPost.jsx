319f202ac460f0fbc5593bacff283c4e1081260d
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-08-02 22:30:59 +0530

    added modals

--- src/Pages/NewPost.jsx
+++ src/Pages/NewPost.jsx
@@ -1,19 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react';
+import SuggestedUser from '../Components/Suggestions/SuggestedUser';
 
 const NewPost = () => {
-  return (
- <div className='ml-[250px]'>
-<button className="btn" onClick={()=>document.getElementById('my_modal_3').showModal()}>open modal</button>
-<dialog id="my_modal_3" className="modal">
-  <div className="modal-box">
-    <form method="dialog">
-      {/* if there is a button in form, it will close the modal */}
-      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-    </form>
-    
-  </div>
-</dialog></div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default NewPost
+  const handleOpenModal = () => {
+    setIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="md:ml-[250px] flex  justify-center md:w-[60vw] w-[100vw]">
+      <button className="btn absolute" onClick={handleOpenModal}>
+        Open modal
+      </button>
+      {isOpen && (
+        <dialog
+          id="my_modal_3"
+          className=" modal relative mt-10 flex h-fit overflow-hidden bg-red-600 w-[90vw] md:w-[400px]  justify-center items-start"
+          open
+        >
+          <div className="border-white border px-6 py-4 border-opacity-60">
+            <form>
+              <div className="flex justify-between items-baseline">
+                <h1 className="text-xl font-semibold">Create a post.</h1>
+                <button
+                  className="btn btn-xl btn-circle btn-ghost"
+                  onClick={handleCloseModal}
+                >
+                  ✕
+                </button>
+              </div>
+
+              <div className="flex flex-col gap-5 mt-5">
+                <input
+                  type="text"
+                  className="w-[300px] md:w-[300px] p-2"
+                  placeholder="Write a caption"
+                />
+                <input
+                  type="file"
+                  accept="image/*"
+                  className="border-l-indigo-400"
+                />
+                <button
+                  type="button"
+                  className="bg-gray-400 text-black color-change  max-w-fit py-2 px-10 rounded-full"
+                >
+                  Create a post
+                </button>
+              </div>
+            </form>
+          </div>
+        </dialog>
+      )}
+    </div>
+  );
+};
+
+export default NewPost;
--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,18 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SuggestedUser from '../Components/Suggestions/SuggestedUser'
 
 const Search = () => {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsSearchOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsSearchOpen(false);
+  };
   return (
-    <div className='ml-[250px]'>
-    <button className="btn" onClick={()=>document.getElementById('my_modal_3').showModal()}>open modal</button>
-    <dialog id="my_modal_3" className="modal">
-      <div className="modal-box">
+    <div className='md:ml-[250px] w-[100vw] flex justify-center md:w-[60vw]'>
+    <button className="btn absolute" onClick={handleOpenModal}>
+        Open modal
+    
+    
+    </button>
+
+    {isSearchOpen && (
+    <dialog id="my_modal_3"
+     className=" modal relative mt-10 flex h-fit bg-red-600 w-[300px] md:w-[370px]  justify-center items-start " open>
+      <div className="border-white  border px-6 py-4 border-opacity-60">
         <form method="dialog">
-          {/* if there is a button in form, it will close the modal */}
-          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+          <div className='flex justify-between items-baseline'>
+             <h1 className='text-xl font-semibold'>Search User</h1>
+          <button className="btn btn-xl btn-circle btn-ghost "
+            onClick={handleCloseModal}>
+              ✕
+          </button>
+
+          </div>
+         
+          
+          <div className='flex flex-col gap-5 mt-5' >
+          <input 
+            type="text" 
+            className='w-[250px] md:w-[300px] p-2'
+             placeholder='Username'/>
+          
+          <button type="button" className=' bg-gray-400 py-2 px-4 rounded-full'>Search</button>
+
+          <SuggestedUser/>
+          </div>
         </form>
         
       </div>
-    </dialog></div>
+    </dialog>)}
+    </div>
   )
 }
 
