8f27e30fa2e27a954aa1ac26eb20b8881ff1a30c
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-08-12 16:01:53 +0530

    fix : bug

--- src/hooks/useDeletePost.jsx
+++ src/hooks/useDeletePost.jsx
@@ -1,17 +1,18 @@
 import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 import { firestore } from "../firebase";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../redux/loggedInUserSlice";
 
 
 const useDeletePost = () => {
-    let loggedInUser = useSelector((store) =>(store.loggedInUser))
+    let loggedInUser = useSelector((store) =>(store.loggedInUser));
+    let dispatch = useDispatch();
     let handleDeletePost = async(postId) =>{ 
         // uid ke basis pr post ko delete krdo 
         await deleteDoc(doc(firestore, "posts", postId));
         console.log("delete ho gya !!");
 
-        let updatedPosts = loggedInUser.posts.filter((post)=>(post!==postId));
+        let updatedPosts = loggedInUser.posts.filter((post)=>(post !== postId));
         const userRef = doc(firestore, "users", loggedInUser.uid);
         // updating post
         await updateDoc(userRef, {posts:updatedPosts});
